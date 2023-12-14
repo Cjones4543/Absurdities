1b25a4291464c3b6f1b6f70f9bc3dccf2f2ba4e0
Cjones4543 <CJones4543@gmail.com>
2023-12-13 19:25:56 -0700

    Update absurd.java

--- Absurdities/src/absurd.java
+++ Absurdities/src/absurd.java
@@ -3,7 +3,7 @@ public class absurd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	System.out.println("Hello World. Soon this will be the Absurdities store :)");
+	System.out.println("Hello World. Soon this will be the Absurdities store :)!!");
 	}
 
 }
