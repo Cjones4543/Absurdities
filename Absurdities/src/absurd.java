57cfa0253a799f2d609fd454609685a0c41aefa5
Cjones4543 <CJones4543@gmail.com>
2023-12-13 19:20:28 -0700

    The start

---
+++ Absurdities/src/absurd.java
@@ -0,0 +1,9 @@
+
+public class absurd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Hello World. Soon this will be the Absurdities store :)");
+	}
+
+}
